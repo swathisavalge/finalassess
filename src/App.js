539ab3cdea43416403866175070491c982c93977
Swathi Savalge <ssavalge@walmartlabs.com>
2021-03-18 18:10:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [photosInfo, setPhotosInfo] = useState([]);
+  
+  useEffect(() => {
+    const data = async () => {
+       await fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+        .then(response  => response.json())
+        .then(data => setPhotosInfo([...data]))
+    }
+    data();
+  },[]);
+
+  const handlePhotoLoad = photosInfo.map((photoInfo)  => {
+    const {id, title, url, thumbnailUrl} = photoInfo;
+    return( 
+        <div className="photo" value={title}  key={id}>
+          <a href={url}>
+            <img src={thumbnailUrl} alt={title}></img>
+          </a>
+          <p className="picTitles">{title}</p>
+        </div>
+    )
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>:) I am Dev Ready :)</h1>
+      <h2>Checkout wide variety of color blocks</h2>
+      <div className="frame">
+        {handlePhotoLoad}
+      </div>
+      
+
     </div>
   );
 }
